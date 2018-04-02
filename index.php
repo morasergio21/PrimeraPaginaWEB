939880bada12c587514118cef0d03cbba6ed3b68
Estudiantes <Estudiantes@Labing50720.udistrital.edu.co>
2018-04-02 17:53:15 -0500

    primercommit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <link href="Estilo.css" rel="stylesheet" type="text/css"/>
+        <title>LOGINUD</title>
+    </head>
+    <body>
+        <p class ="titulo">
+            Hola bienvenidos a mi primer sitio
+        </p>
+        <form action ="procesador">
+            <input type="text" name="usuario"><br>
+            <input type="password" name="clave"><br>
+            <input type="submit" name="boton" value="ingresar">
+        </form>
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
